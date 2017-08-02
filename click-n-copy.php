48aee0f5b3880561350a9b629bc2c81a3419fa56
Harry Jackson <harryjackson1221@gmail.com>
2017-08-02 11:52:36 -0400

    Create click-n-copy.php
    Added click2copy

---
+++ click-n-copy.php
@@ -0,0 +1,11 @@
+<?php
+   /*
+   Plugin Name: Click2Copy
+   Plugin URI: https://github.com/harryjackson1221/click-n-copy
+   Description: A plugin to rule them all and add clipboard.js to WordPress
+   Version: 0.1
+   Author: Harry Jackson
+   Author URI: http://harryj.us
+   License: GPL2
+   */
+?>
