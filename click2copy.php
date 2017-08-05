f16a78628ebc5a1d37e601219cc613ca6e9ac266
Harry Jackson <harryjackson1221@gmail.com>
2017-08-05 13:12:41 -0400

    Update click2copy.php
    Decided on running content through htmlspecialchars, the only problem is double quotes that do not exist within tags are showing as escaped - Currently researching ways to replace this with javascript, but may be stuck here

--- click2copy.php
+++ click2copy.php
@@ -15,7 +15,7 @@ defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
 remove_filter('the_content', 'wpautop');
 
-wp_register_script( 'clipboardjs', 'https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/1.7.1/clipboard.min.js' , array() , '1.7' );
+wp_register_script( 'clipboardjs', 'https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/1.7.1/clipboard.min.js' , array() , '1.7', 'all' );
 wp_enqueue_script('clipboardjs');
 wp_register_script('copyjs', plugins_url('/js/copy.js', __FILE__), false, '1.0', 'all');
 wp_enqueue_script( 'copyjs');
@@ -27,23 +27,24 @@ add_shortcode('c2c', 'Click2Copy');
 function Click2Copy($atts, $content) {
 //echo "<pre>\n";
 //echo "attr: [";
-//print_r($atts);
+//print_r($content, ENT_NOQUOTES);
 //echo "]\n";
 //echo "content: [";
 //print_r($content);
 //echo "]";
 //echo "</pre>\n";
-//$escaped_copytext = htmlEntities( $content , ENT_NOQUOTES , ENT_HTML5 );
 $escaped_copytext = htmlentities( $content );
-$unescaped_copytext = html_entity_decode( $escaped_copytext );
-return '<pre id="' . $atts['id'] . '">' . $escaped_copytext . '</pre><br/><center><button style="text-align: center;" class="btn ' . $atts['class'] . '" data-clipboard-target="pre#' . $atts['id'] . '">
+$escaped_copytext2 = htmlspecialchars( $content );
+//$unescaped_copytext = html_entity_decode( $content );
+return '<pre id="' . $atts['id'] . '" class="' . $atts['pclass'] . '">' . $escaped_copytext2 . '</pre><br/><center>
+<button style="text-align: center;" class="' . $atts['bclass'] . '" data-clipboard-target="pre#' . $atts['id'] . '">
     ' . $atts['button-text'] . '
 </button></center>';
 }
 
 
-//usage
-//[c2c id="code1" class="btn" button-text="Copy"]
+//usage - the text between [] [/] is the content that will be copied
+//[c2c id="code1" pre-class="" button-class="btn" button-text="Copy"]
 //a href="//link.com">link</a>
 //<a href="//link.com">link</a>
 //I can output HTML here and CSS
