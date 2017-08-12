4ff91f357acafedc6279f452d7f71d170c9735d0
Harry Jackson <harryjackson1221@gmail.com>
2017-08-12 11:32:17 -0400

    Update click2copy.php
    Added function and filter to create admin page

--- click2copy.php
+++ click2copy.php
@@ -21,6 +21,27 @@ wp_register_script('copyjs', plugins_url('/js/copy.js', __FILE__), false, '1.0',
 wp_enqueue_script( 'copyjs');
 wp_register_style('click2copy', plugins_url('/css/copy.css', __FILE__), false, '1.0', 'all');
 wp_enqueue_style( 'click2copy');
+
+/** Add the admin menu, using my function */
+add_action( 'admin_menu', 'my_plugin_menu' );
+
+/** Function to set my options */
+function my_plugin_menu() {
+	add_options_page( 'My Plugin Options', 'My Plugin', 'manage_options', 'my-unique-identifier', 'my_plugin_options' );
+}
+
+/** The page to add forms for setting the things */
+function my_plugin_options() {
+	if ( !current_user_can( 'manage_options' ) )  {
+		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+	}
+	echo '<div class="wrap">';
+	echo '<p>Here is where the form would go if I actually had options.</p>';
+	echo '</div>';
+}
+
+
+
 //lets add a shortcode to echo and copy the content tag in a shortcode
 add_shortcode('c2c', 'Click2Copy');
 
