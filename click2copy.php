3a5099f0bcea8d1267f7e03395a9f14ae9c11f15
Harry Jackson <harryjackson1221@gmail.com>
2017-09-01 08:25:53 -0400

    Update click2copy.php
    Added filter to remove wp_texturize from shortcode to stop double quotes from being escaped within HTML tags in output

--- click2copy.php
+++ click2copy.php
@@ -22,29 +22,13 @@ wp_enqueue_script( 'copyjs');
 wp_register_style('click2copy', plugins_url('/css/copy.css', __FILE__), false, '1.0', 'all');
 wp_enqueue_style( 'click2copy');
 
-/** Add the admin menu, using my function */
-add_action( 'admin_menu', 'my_plugin_menu' );
-/** Function to set my options */
-function my_plugin_menu() {
-        add_menu_page( 'Click2Copy Options', 'Click2Copy', 'manage_options', 'click2copy', 'my_plugin_options', '', '10' );
-}
-/** The page to add forms for setting the things */
-function my_plugin_options() {
-        if ( !current_user_can( 'manage_options' ) )  {
-                wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
-        }
-        echo '<div class="wrap">';
-        echo '<h2>Click2Copy Options</h2>';
-        echo '<img src="https://www.boldgrid.com/assets/logos/boldgrid-logo-horizontal-black-lg.png" width="50%">';
-        echo'<p>Here is where the form would go if I actually had options.</p>';
-        echo '</div>';
-}
 
-function append2_footer_admin () {
-echo '<i class="footer-custom">| Made awesome by <a href="//harryj.us" target="_blank">Harry</a>.</i>';
-}
-add_filter('admin_footer_text', 'append2_footer_admin');
 
+add_filter( 'no_texturize_shortcodes', 'shortcodes_to_exempt_from_wptexturize' );
+function shortcodes_to_exempt_from_wptexturize( $shortcodes ) {
+    $shortcodes[] = 'c2c';
+    return $shortcodes;
+}
 
 
 //lets add a shortcode to echo and copy the content tag in a shortcode
@@ -52,18 +36,9 @@ add_shortcode('c2c', 'Click2Copy');
 
 //Set the function to allow for the shortcode
 function Click2Copy($atts, $content) {
-//echo "<pre>\n";
-//echo "attr: [";
-//print_r($content, ENT_NOQUOTES);
-//echo "]\n";
-//echo "content: [";
-//print_r($content);
-//echo "]";
-//echo "</pre>\n";
-$escaped_copytext = htmlentities( $content );
-$escaped_copytext2 = htmlspecialchars( $content );
+$escaped_copytext = htmlspecialchars( $content );
 //$unescaped_copytext = html_entity_decode( $content );
-return '<pre id="' . $atts['id'] . '" class="' . $atts['pclass'] . '">' . $escaped_copytext2 . '</pre><br/><center>
+return '<pre id="' . $atts['id'] . '" class="' . $atts['pclass'] . '">' . $escaped_copytext . '</pre><br/><center>
 <button style="text-align: center;" class="' . $atts['bclass'] . '" data-clipboard-target="pre#' . $atts['id'] . '">
     ' . $atts['button-text'] . '
 </button></center>';
