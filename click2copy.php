993ebaa3c5112e11a13e866a749e36eeeeabea0b
harryjackson1221 <harryjackson1221@gmail.com>
2017-10-18 22:58:16 -0400

    Added get_options to shortcode output

--- click2copy.php
+++ click2copy.php
@@ -32,58 +32,134 @@ defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 wp_register_script( 'clipboardjs', 'https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/1.7.1/clipboard.min.js' , array() , '1.7', 'all' );
 wp_enqueue_script('clipboardjs');
 
-wp_register_script('copyjs', plugins_url('/js/copy.js', __FILE__), false, '0.3', 'all');
+wp_register_script('copyjs', plugins_url('/js/copy.js', __FILE__), false, '0.4', 'all');
 wp_enqueue_script( 'copyjs');
 
-wp_register_style('click2copy', plugins_url('/css/copy.css', __FILE__), false, '0.3', 'all');
+wp_register_style('click2copy', plugins_url('/css/copy.css', __FILE__), false, '0.4', 'all');
 wp_enqueue_style( 'click2copy');
 
-/** Add the admin menu, using my function */
-add_action( 'admin_menu', 'c2c_plugin_menu' );
-
-/** Function to set my options */
-function c2c_plugin_menu() {
-	add_options_page( 'click2copy Options', 'click2copy', 'manage_options', 'click2copy', 'c2c_plugin_options');
-}
-
 function c2c_custom_option_logo() {
-    echo '<style type="text/css">.c2c:before {content: url(' . plugins_url( 'images/logo-small.png', __FILE__ ) . ') !important;background-repeat: no-repeat;position:relative;top:8px;}</style>';
+    echo '<style type="text/css">.c2c:before {content: url(' . plugins_url( 'images/logo-small.png', __FILE__ ) . ') !important;background-repeat: no-repeat;position:relative;top:5px;}</style>';
 }
 
 //hook into the administrative header output
 add_action('wp_before_admin_bar_render', 'c2c_custom_option_logo');
 
 /** add plugin settings */
+add_action( 'admin_menu', 'c2c__add_admin_menu' );
+add_action( 'admin_init', 'c2c__settings_init' );
 
-function c2c_plugin_settings_link($links) { 
-  $settings_link = '<a href="options-general.php?page=click2copy">Settings</a>';
-  array_unshift($links, $settings_link); 
-  return $links; 
+
+function c2c__add_admin_menu(  ) { 
+    add_options_page( 'click2copy', 'click2copy', 'manage_options', 'click2copy', 'c2c__options_page' );
 }
- 
-$plugin = plugin_basename(__FILE__); 
-add_filter("plugin_action_links_$plugin", 'c2c_plugin_settings_link' );
 
-/** The page to add forms for setting the things soon */
-function c2c_plugin_options() {
-	if ( !current_user_can( 'manage_options' ) )  {
-		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
-	}
-	
-    echo '<div class="wrap">';
 
-    echo '<h2 class="c2c"> click2copy</h2>
+function c2c__settings_init(  ) { 
+
+    register_setting( 'pluginPage', 'c2c__settings' );
+
+    add_settings_section(
+        'c2c__pluginPage_section', 
+        __( 'Global Options', 'click2copy' ), 
+        'c2c__settings_section_callback', 
+        'pluginPage'
+    );
+
+    add_settings_field( 
+        'c2c__text_field_0', 
+        __( '&lt;pre&gt; css class', 'click2copy' ), 
+        'c2c__text_field_0_render', 
+        'pluginPage', 
+        'c2c__pluginPage_section' 
+    );
+
+    add_settings_field( 
+        'c2c__text_field_1', 
+        __( 'Button CSS Class', 'click2copy' ), 
+        'c2c__text_field_1_render', 
+        'pluginPage', 
+        'c2c__pluginPage_section' 
+    );
+
+    add_settings_field( 
+        'c2c__text_field_2', 
+        __( 'Button text', 'click2copy' ), 
+        'c2c__text_field_2_render', 
+        'pluginPage', 
+        'c2c__pluginPage_section' 
+    );
+
+
+}
+
+
+function c2c__text_field_0_render(  ) { 
+
+    $options = get_option( 'c2c__settings' );
+    ?>
+    <input type='text' name='c2c__settings[c2c__text_field_0]' placeholder="c2cpre" value='<?php echo $options['c2c__text_field_0']; ?>'>
+    <?php
+
+}
+
+
+function c2c__text_field_1_render(  ) { 
+
+    $options = get_option( 'c2c__settings' );
+    ?>
+    <input type='text' name='c2c__settings[c2c__text_field_1]' placeholder="button-primary" value='<?php echo $options['c2c__text_field_1']; ?>'>
+    <?php
+
+}
+
+
+function c2c__text_field_2_render(  ) { 
+
+    $options = get_option( 'c2c__settings' );
+    ?>
+    <input type='text' name='c2c__settings[c2c__text_field_2]' placeholder="Copy" value='<?php echo $options['c2c__text_field_2']; ?>'>
+    <?php
+
+}
+
+
+function c2c__settings_section_callback(  ) { 
+
+    echo __( 'This section description', 'click2copy' );
+
+}
+
+
+function c2c__options_page(  ) { 
+        if ( !current_user_can( 'manage_options' ) )  {
+        wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+    }
+
+    ?>
+    <form action='options.php' method='post'>
+
+        <?php    echo '<h1 class="c2c"> click2copy</h1>
         <p><h2>Usage</h2><p>The shortcode can be applied in either the Page or  Post Editor using the following syntax: [c2c id="" pclass="" button-text="Text"]content[/c2c]</p>
         <h2>Options</h2>
         <p><ul>
-        <li><b>id:</b> This identifies the code being copied, and only needs to be used if you have more than one instance on the page/post</li> 
+        <li><b>id:</b> This identifies the code being copied, and only needs to be unique for each snippet if you have more than one instance on the page/post</li> 
         <li><b>pclass:</b> This applies a custom class to the &lt;pre&gt; HTML tag for styling purposes</li> 
         <li><b>bclass:</b> This applies a custom CSS class to the &lt;button&gt; HTML tag for styling purposes</li> 
         <li><b>button-text:</b> This allows you to set the button text</li> 
         </ul></p>';
 
-    echo '<h2>Example Use</h2>
-        <p>[c2c id=&quot;code1&quot; pclass=&quot;pre2&quot; bclass=&quot;btn&quot; button-text=&quot;Copy&quot;]<br/>
+        settings_fields( 'pluginPage' );
+        do_settings_sections( 'pluginPage' );
+        submit_button();
+        ?>
+
+    </form>
+
+
+    <?php
+     echo '<h2>Example Use</h2>
+        <p>[c2c id=&quot;code1&quot;]<br/>
         &lt;html&gt;&lt;/html&gt; <br/>
         &lt;?php ?&gt; <br/>
         .css { display:code; }<br/>
@@ -97,9 +173,11 @@ function c2c_plugin_options() {
         <li><a href="https://github.com/harryjackson1221/click2copy" target="_blank">click2copy</a> - the project</li>
         </ul>
         </p>';
-	
-	echo '</div>';
+    
+    echo '';
 }
+ 
+ 
 
 // stop the curly quotes in the shortcode c2c
 add_filter( 'no_texturize_shortcodes', 'shortcodes_to_exempt_from_wptexturize' );
@@ -114,8 +192,13 @@ add_shortcode('c2c', 'click2copy');
 
 // set the function for the shortcode
 function click2copy($atts, $content) {
+
     $escaped_copytext = htmlspecialchars( "$content" );
-    return '<pre id="' . $atts['id'] . '" class="' . $atts['pclass'] . '">' . $escaped_copytext . '</pre><button class="' . $atts['bclass'] . '" data-clipboard-target="pre#' . $atts['id'] . '">' . $atts['button-text'] . '</button>';
+
+    $c2c_options = get_option( 'c2c__settings' );
+
+    return '<pre id="' . $atts['id'] . '" class="' . $c2c_options['c2c__text_field_0'] . '">' . $escaped_copytext . '</pre><button class="' . $c2c_options['c2c__text_field_1'] . '" data-clipboard-target="pre#' . $atts['id'] . '">' . $c2c_options['c2c__text_field_2'] . '</button>';
 }
+
 
 ?>
