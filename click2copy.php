abf0d5449de62aedf8e7077ccf57fb05e708af11
harryjackson1221 <harryjackson1221@gmail.com>
2017-10-25 08:00:13 -0400

    defined blank variables with empty() and added defaults

--- click2copy.php
+++ click2copy.php
@@ -3,7 +3,7 @@
 Plugin Name: click2copy
 Plugin URI: https://github.com/harryjackson1221/click2copy
 Description: One Plugin to Rule Them All! and it also adds clipboard.js functionality to WordPress, to copy code snippets and create smiles :-)
-Version: 0.3
+Version: 1.0
 Author: Harry Jackson
 Author URI: http://harryj.us
 License: GPL2
@@ -38,14 +38,15 @@ wp_enqueue_script( 'copyjs');
 wp_register_style('click2copy', plugins_url('/css/copy.css', __FILE__), false, '0.4', 'all');
 wp_enqueue_style( 'click2copy');
 
+
 function c2c_custom_option_logo() {
     echo '<style type="text/css">.c2c:before {content: url(' . plugins_url( 'images/logo-small.png', __FILE__ ) . ') !important;background-repeat: no-repeat;position:relative;top:5px;}</style>';
 }
 
-//hook into the administrative header output
+//hook into the administrative header output, so we can add the logo :before
 add_action('wp_before_admin_bar_render', 'c2c_custom_option_logo');
 
-/** add plugin settings */
+/** add plugin settings page **/
 add_action( 'admin_menu', 'c2c__add_admin_menu' );
 add_action( 'admin_init', 'c2c__settings_init' );
 
@@ -54,21 +55,20 @@ function c2c__add_admin_menu(  ) {
     add_options_page( 'click2copy', 'click2copy', 'manage_options', 'click2copy', 'c2c__options_page' );
 }
 
-
 function c2c__settings_init(  ) { 
 
-    register_setting( 'pluginPage', 'c2c__settings' );
+    register_setting( 'pluginPage', 'c2c__settings');
 
     add_settings_section(
         'c2c__pluginPage_section', 
-        __( 'Global Options', 'click2copy' ), 
+        __( 'click2copy Options', 'click2copy' ), 
         'c2c__settings_section_callback', 
         'pluginPage'
     );
 
     add_settings_field( 
         'c2c__text_field_0', 
-        __( '&lt;pre&gt; css class', 'click2copy' ), 
+        __( '&lt;pre&gt; CSS class', 'click2copy' ), 
         'c2c__text_field_0_render', 
         'pluginPage', 
         'c2c__pluginPage_section' 
@@ -76,7 +76,7 @@ function c2c__settings_init(  ) {
 
     add_settings_field( 
         'c2c__text_field_1', 
-        __( 'Button CSS Class', 'click2copy' ), 
+        __( '&lt;button&gt; CSS Class', 'click2copy' ), 
         'c2c__text_field_1_render', 
         'pluginPage', 
         'c2c__pluginPage_section' 
@@ -84,53 +84,45 @@ function c2c__settings_init(  ) {
 
     add_settings_field( 
         'c2c__text_field_2', 
-        __( 'Button text', 'click2copy' ), 
+        __( '&lt;button&gt; Text', 'click2copy' ), 
         'c2c__text_field_2_render', 
         'pluginPage', 
         'c2c__pluginPage_section' 
     );
-
-
 }
-
 
 function c2c__text_field_0_render(  ) { 
 
     $options = get_option( 'c2c__settings' );
+
     ?>
     <input type='text' name='c2c__settings[c2c__text_field_0]' placeholder="c2cpre" value='<?php echo $options['c2c__text_field_0']; ?>'>
     <?php
-
 }
-
 
 function c2c__text_field_1_render(  ) { 
 
-    $options = get_option( 'c2c__settings' );
+    $options = get_option( 'c2c__settings'  );
     ?>
     <input type='text' name='c2c__settings[c2c__text_field_1]' placeholder="button-primary" value='<?php echo $options['c2c__text_field_1']; ?>'>
     <?php
-
 }
-
 
 function c2c__text_field_2_render(  ) { 
 
-    $options = get_option( 'c2c__settings' );
+    $options = get_option( 'c2c__settings'  );
+
     ?>
     <input type='text' name='c2c__settings[c2c__text_field_2]' placeholder="Copy" value='<?php echo $options['c2c__text_field_2']; ?>'>
     <?php
-
 }
-
 
 function c2c__settings_section_callback(  ) { 
 
-    echo __( 'This section description', 'click2copy' );
-
+    echo __( '<p><ul><li><b>id:</b> This identifies the code being copied, and needs to be unique for each snippet you have on the page/post. This is set within the shortcode as id="unique ID" and allows the button to target the correct element</li><li><b>pclass:</b> This applies a custom CSS class to the &lt;pre&gt; HTML tag for styling purposes</li><li><b>bclass:</b> This applies a custom CSS class to the &lt;button&gt; HTML tag for styling purposes</li><li><b>button-text:</b> This allows you to set the button text</li></ul></p>' , 'click2copy' );
 }
 
-
+// Create the settings page content
 function c2c__options_page(  ) { 
         if ( !current_user_can( 'manage_options' ) )  {
         wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
@@ -139,15 +131,7 @@ function c2c__options_page(  ) {
     ?>
     <form action='options.php' method='post'>
 
-        <?php    echo '<h1 class="c2c"> click2copy</h1>
-        <p><h2>Usage</h2><p>The shortcode can be applied in either the Page or  Post Editor using the following syntax: [c2c id="" pclass="" button-text="Text"]content[/c2c]</p>
-        <h2>Options</h2>
-        <p><ul>
-        <li><b>id:</b> This identifies the code being copied, and only needs to be unique for each snippet if you have more than one instance on the page/post</li> 
-        <li><b>pclass:</b> This applies a custom class to the &lt;pre&gt; HTML tag for styling purposes</li> 
-        <li><b>bclass:</b> This applies a custom CSS class to the &lt;button&gt; HTML tag for styling purposes</li> 
-        <li><b>button-text:</b> This allows you to set the button text</li> 
-        </ul></p>';
+        <?php  echo __('<h1 class="c2c"> click2copy</h1><p><h2>Usage</h2><p>The shortcode can be applied in either the Page or  Post Editor using the following syntax: [c2c id="unique-ID"]content[/c2c]</p>');
 
         settings_fields( 'pluginPage' );
         do_settings_sections( 'pluginPage' );
@@ -156,28 +140,9 @@ function c2c__options_page(  ) {
 
     </form>
 
-
-    <?php
-     echo '<h2>Example Use</h2>
-        <p>[c2c id=&quot;code1&quot;]<br/>
-        &lt;html&gt;&lt;/html&gt; <br/>
-        &lt;?php ?&gt; <br/>
-        .css { display:code; }<br/>
-        [/c2c]<br/>
-        </p>';
-        
-    echo '<h2>Built With</h2>
-        <ul>
-        <li><a href="https://clipboardjs.com/" target="_blank">clipboard.js</a> - the JavaScript (thanks!)</li>
-        <li><a href="http://harryj.us/" target="_blank">harryj.us</a> - the person</li>
-        <li><a href="https://github.com/harryjackson1221/click2copy" target="_blank">click2copy</a> - the project</li>
-        </ul>
-        </p>';
-    
-    echo '';
-}
- 
- 
+        <?php  echo __('<h2>Example Use</h2><p>[c2c id=&quot;code1&quot;]<br/>&lt;html&gt;&lt;/html&gt; <br/>&lt;?php ?&gt; <br/>.css { display:code; }<br/>[/c2c]<br/></p>     
+        <h2>Built With</h2><ul><li><a href="https://clipboardjs.com/" target="_blank">clipboard.js</a> - the JavaScript (thanks!)</li><li><a href="http://harryj.us/" target="_blank">harryj.us</a> - the person</li><li><a href="https://github.com/harryjackson1221/click2copy" target="_blank">click2copy</a> - the project</li></ul></p>');
+} 
 
 // stop the curly quotes in the shortcode c2c
 add_filter( 'no_texturize_shortcodes', 'shortcodes_to_exempt_from_wptexturize' );
@@ -187,18 +152,34 @@ function shortcodes_to_exempt_from_wptexturize( $shortcodes ) {
     return $shortcodes;
 }
 
+//autop adds some line breaks so remove it, add it back with a higher priority 
+remove_filter('the_content', 'wpautop');
+add_filter('the_content', 'wpautop','99');
+
 // add a shortcode to echo and copy the content tag in a shortcode
 add_shortcode('c2c', 'click2copy');
 
 // set the function for the shortcode
 function click2copy($atts, $content) {
+// Create default options for the array, in case someone doesnt set an option
+
+
+    $c2c_options = get_option('c2c__settings');
+
+    //set constants for options, and defaults for empty fields
+    $c2c_pre = $c2c_options['c2c__text_field_0'];
+    if ( empty($c2c_pre) ) $c2c_pre = 'c2cpre';
+
+    $c2c_button = $c2c_options['c2c__text_field_1'];
+    if ( empty($c2c_button) ) $c2c_button = 'button-primary';
+
+    $c2c_btn_text = $c2c_options['c2c__text_field_2'];
+    if ( empty($c2c_btn_text) ) $c2c_btn_text = 'Click';
 
     $escaped_copytext = htmlspecialchars( "$content" );
 
-    $c2c_options = get_option( 'c2c__settings' );
-
-    return '<pre id="' . $atts['id'] . '" class="' . $c2c_options['c2c__text_field_0'] . '">' . $escaped_copytext . '</pre><button class="' . $c2c_options['c2c__text_field_1'] . '" data-clipboard-target="pre#' . $atts['id'] . '">' . $c2c_options['c2c__text_field_2'] . '</button>';
+// Return the stuff
+    return '<pre id="' . $atts['id'] . '" class="' . $c2c_pre . '">' . $escaped_copytext . '</pre><button data-toggle="tooltip" class="' . $c2c_button . '" data-clipboard-target="pre#' . $atts['id'] . '">' . $c2c_btn_text . '</button>';
 }
-
 
 ?>
--- js/copy.js
+++ js/copy.js
@@ -1,8 +1,15 @@
+   
+
+
     var c2cbtns = document.querySelectorAll('button');
+    
     var clipboard = new Clipboard(c2cbtns);
+
+        
     clipboard.on('success', function(e) {
-        console.log(e);
+        console.log(e); 
     });
+    
     clipboard.on('error', function(e) {
         console.log(e);
     });
